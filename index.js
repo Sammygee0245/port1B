b9294f08085b16215c282bc4e0e09a9a4ef0a8ef
Samuel Bediako <samuelbediako020.com>
2023-01-12 16:43:24 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let bars = document.querySelector(".bars")
+
+let unorderedList2  =  document.querySelector(".unordered-list2")
+
+let isToggle = false
+
+// adding clicked event to hambuger menu
+
+bars.addEventListener("click",()=>{
+    if(!isToggle){
+         unorderedList2.style.left = "0"
+        isToggle = true
+    }else{
+       unorderedList2.style.left = "-1000px"
+        isToggle = false
+    }
+})
+
+unorderedList2.addEventListener("click",()=>{
+     unorderedList2.style.left = "-1000px"
+     isToggle = false
+})
