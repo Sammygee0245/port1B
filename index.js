7ab665128be4d67e89e334fad418a87bf3616514
sammygee0245 <samuelbediako020@gmail.com>
2023-02-27 20:15:54 +0000

    updated

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-let bars = document.querySelector(".bars")
+let bars = document.querySelector(".bars");
 
-let unorderedList2  =  document.querySelector(".unordered-list2")
+let unorderedList2 = document.querySelector(".unordered-list2");
 
-let isToggle = false
+let isToggle = false;
 
 // adding clicked event to hambuger menu
 
-bars.addEventListener("click",()=>{
-    if(!isToggle){
-         unorderedList2.style.left = "0"
-        isToggle = true
-    }else{
-       unorderedList2.style.left = "-1000px"
-        isToggle = false
-    }
-})
+bars.addEventListener("click", () => {
+  if (!isToggle) {
+    unorderedList2.style.left = "0";
+    isToggle = true;
+  } else {
+    unorderedList2.style.left = "-1000px";
+    isToggle = false;
+  }
+});
 
-unorderedList2.addEventListener("click",()=>{
-     unorderedList2.style.left = "-1000px"
-     isToggle = false
-})
+unorderedList2.addEventListener("click", () => {
+  unorderedList2.style.left = "-1000px";
+  isToggle = false;
+});
+
+// Swipper js
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+});
